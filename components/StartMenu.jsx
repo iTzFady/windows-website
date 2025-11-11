47053ea7218178e6d6b8e3db8a19338f89222f4c
Faaady <47241432+iTzFady@users.noreply.github.com>
2025-11-11 17:30:20 +0200

    Improved Start menu

--- components/StartMenu.jsx
+++ components/StartMenu.jsx
@@ -1,3 +1,4 @@
+import { Fragment, useState } from "react";
 import {
   FaUserCircle,
   FaPowerOff,
@@ -7,40 +8,78 @@ import {
 } from "react-icons/fa";
 import { CiMenuBurger } from "react-icons/ci";
 
+const programs = [
+  { name: "Zoom", icon: <FaFolder /> },
+  { name: "Excel", icon: <FaFolder /> },
+  { name: "Word", icon: <FaFolder /> },
+  { name: "Photoshop", icon: <FaFolder /> },
+  { name: "Illustrator", icon: <FaFolder /> },
+  { name: "Chrome", icon: <FaFolder /> },
+  { name: "After Effects", icon: <FaFolder /> },
+  { name: "Premiere Pro", icon: <FaFolder /> },
+  { name: "Notepad", icon: <FaFolder /> },
+  { name: "Access", icon: <FaFolder /> },
+];
+
+function groupByFirstLetter(items) {
+  const sorted = [...items].sort((a, b) => a.name.localeCompare(b.name));
+  console.log(items);
+  return sorted.reduce((groups, item) => {
+    const letter = item.name[0].toUpperCase();
+    if (!groups[letter]) {
+      groups[letter] = [];
+    }
+    groups[letter].push(item);
+    return groups;
+  }, {});
+}
+
 export default function StartMenu() {
+  const [collapsed, setCollapsed] = useState(true);
+  const groupedPrograms = groupByFirstLetter(programs);
+
   return (
     <div className="start-menu">
-      <div className="start-left">
+      <div className={`start-left ${collapsed ? "collapsed" : ""}`}>
         <div className="top-start">
-          <button className="top-button">
+          <button
+            className="top-button"
+            onClick={() => setCollapsed(!collapsed)}
+          >
             <CiMenuBurger className="top-icon" />
           </button>
-          <div className="top-label">Start</div>
+          {!collapsed && <span>Start</span>}
         </div>
 
         <ul className="quick-links">
           <div className="user-profile">
             <FaUserCircle className="user-icon" />
-            <div className="user-name">Adminstrator</div>
+            {!collapsed && <div className="user-name">Administrator</div>}
           </div>
           <li>
-            <FaCog /> Settings
+            <FaCog />
+            {!collapsed && <span>Settings</span>}
           </li>
           <li>
-            <FaPowerOff /> Power
+            <FaPowerOff />
+            {!collapsed && <span>Power</span>}
           </li>
         </ul>
       </div>
-
       <div className="start-right">
-        <div className="tile-grid">
-          <div className="tile blue large">Mail</div>
-          <div className="tile gray">Microsoft Edge</div>
-          <div className="tile green">Calendar</div>
-          <div className="tile red">Store</div>
-          <div className="tile blue">Weather</div>
-          <div className="tile gray">Photos</div>
-        </div>
+        {Object.keys(groupedPrograms).map((letter) => (
+          <div key={letter}>
+            <span>{letter}</span>
+            <div className="program-list">
+              {groupedPrograms[letter].map((program) => (
+                <div key={program.name} className="program-item">
+                  {program.icon && program.icon}
+                  <span>{program.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/Taskbar.jsx
+++ components/Taskbar.jsx
@@ -49,7 +49,7 @@ export default function Taskbar({
         <div className="system-tray">
           <i className="fa-solid fa-volume-high"></i>
           <i className="fa-solid fa-wifi"></i>
-          <i class="fa-solid fa-battery-quarter"></i>
+          <i className="fa-solid fa-battery-quarter"></i>
         </div>
         <div className="taskbar-clock">{time}</div>
       </div>
